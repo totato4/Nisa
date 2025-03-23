1ec776e23e778e0eb8ecd673e89d660cb9b681ad
totato4 <v4.97@list.ru>
2025-03-23 18:39:08 +0300

    prepared to deploy

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  root: 'src', // Корневая папка проекта
+  build: {
+    outDir: '../dist', // Папка для собранного проекта
+    emptyOutDir: true, // Очищать папку перед сборкой
+    assetsDir: 'assets', // Папка для статических файлов (изображений, шрифтов и т.д.)
+    minify: 'terser', // Минификация кода
+    sourcemap: true, // Генерация sourcemaps для отладки
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'), // Точка входа
+      },
+      output: {
+        assetFileNames: 'assets/[name].[hash].[ext]', // Формат имен файлов
+      },
+    },
+  },
+});
